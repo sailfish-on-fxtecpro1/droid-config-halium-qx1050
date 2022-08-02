1fe6f1af01115c7612e7e1cd63c9ef3026d40ebb
Adam Pigg <adam@piggz.co.uk>
2022-08-02 22:26:31 +0100

    Add service to run after droid-hal-init
    Initially fixes permissions for vibrator

---
+++ sparse/usr/bin/droid/post-droid-hal-init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+chown root:input /sys/class/leds/vibrator/activate
+chown root:input /sys/class/leds/vibrator/brightness
+chown root:input /sys/class/leds/vibrator/duration
+chown root:input /sys/class/leds/vibrator/state
+chown root:input /sys/class/leds/vibrator/trigger

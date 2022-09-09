3c81ca827fbeaefdbced240680ef5dd08cb6861c
Adam Pigg <adam@piggz.co.uk>
2022-09-09 22:43:26 +0100

    Moved service to common droid-common-halium

--- sparse/usr/bin/droid/post-droid-hal-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-chown root:input /sys/class/leds/vibrator/activate
-chown root:input /sys/class/leds/vibrator/brightness
-chown root:input /sys/class/leds/vibrator/duration
-chown root:input /sys/class/leds/vibrator/state
-chown root:input /sys/class/leds/vibrator/trigger
